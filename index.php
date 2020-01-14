7bc682c3a9f8283190b3c38abbd598efed7c584e
igorezov17 <rezaevigor@yandex.ru>
2020-01-14 13:07:14 +0300

    new project mogo

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/style.css"> 
+    <link href="https://fonts.googleapis.com/css?family=Kaushan+Script|Montserrat:400,700&display=swap&subset=cyrillic-ext" rel="stylesheet">>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mogo</title>
+</head>
+<body>
+    <h1>Mogo<h1>
+</body>
+</html>
